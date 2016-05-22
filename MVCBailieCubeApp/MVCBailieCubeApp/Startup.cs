e2f6cb41db80ea2b5101d2adc8c7de5a7c486add
Mason Bailie <mason@grmw.org>
2016-05-21 17:18:46 -0700

    General MVC Setup

---
+++ MVCBailieCubeApp/MVCBailieCubeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCBailieCubeApp.Startup))]
+namespace MVCBailieCubeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
