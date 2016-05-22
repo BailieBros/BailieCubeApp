f7d035b499231e82aa581198124386ac018b6248
sbailie <sbailie@gmail.com>
2016-05-21 19:58:16 -0800

    Created Cards and Cubes Models

---
+++ MVCBailieCubeApp/MVCBailieCubeApp/Models/Cards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBailieCubeApp.Models
+{
+    public class Cards
+    {
+        public int CardID { get; set; }
+        public string CardName { get; set; }
+
+    }
+}
---
+++ MVCBailieCubeApp/MVCBailieCubeApp/Models/Cubes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBailieCubeApp.Models
+{
+    public class Cubes
+    {
+        public int CubeID { get; set; }
+        public string CubeTitle { get; set; }
+        public int UserID { get; set; }
+
+    }
+}
